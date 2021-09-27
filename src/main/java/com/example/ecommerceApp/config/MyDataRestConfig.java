0ff97db69d6b7e315fe2a66efaab3a1f97e7221d
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-27 16:19:54 +0300

    Disable PATCH method

--- src/main/java/com/example/ecommerceApp/config/MyDataRestConfig.java
+++ src/main/java/com/example/ecommerceApp/config/MyDataRestConfig.java
@@ -37,7 +37,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
         // disable HTTP methods for Product: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         // disable HTTP methods for ProductCategory: PUT, POST and DELETE
